58844e744a55568407c16707534b73e938c0e80f
Lionel VEST <lionel.vest@adaris.org>
2024-03-01 18:35:04 +0100

    Allow F1 to F12 and ShiftKey Combination

--- keyboard_shortcuts.js
+++ keyboard_shortcuts.js
@@ -1,158 +1,276 @@
-function keyboard_shortcuts_show_help() {
-  $('#keyboard_shortcuts_help').dialog('open');
+function keyboard_shortcuts_show_help()
+{
+	$('#keyboard_shortcuts_help').dialog('open')
 }
 
-$(function() {
+$(function ()
+{
 
-  // initialize a dialog window
-  $('#keyboard_shortcuts_help').dialog({
-    autoOpen: false,
-    draggable: true,
-    modal: false,
-    resizable: false,
-    width: 750,
-    title: rcmail.gettext("keyboard_shortcuts.keyboard_shortcuts")
-  });
+	// initialize a dialog window
+	$('#keyboard_shortcuts_help').dialog({
+		autoOpen: false,
+		draggable: true,
+		modal: true,
+		resizable: false,
+		width: 950,
+		title: rcmail.gettext("keyboard_shortcuts.keyboard_shortcuts")
+	})
 
-  // fire up the keypress event listener
-  $(document).keypress(function (e) {
-    return key_pressed(e);
-  });
+	// fire up the keypress event listener
+	$(document).keydown(function (e)
+	{
+		return key_pressed(e)
+	})
 
 
-  function key_pressed (e) {
-    console.log(e.which)
-    // special case. If we hit ctrl-enter, and we're composing, and we have focus, then send email
-    if (rcmail.env.action == 'compose' && e.which == 13 && e.ctrlKey && $("*:focus").is("#composebody")) {
-      $('.button.send').click();
-      return false;
-    }
+	function key_pressed(e)
+	{
+		// special case. If we hit ctrl-enter, and we're composing, and we have focus, then send email
+		if (rcmail.env.action == 'compose' && e.which == 13 && e.ctrlKey && $("*:focus").is("#composebody"))
+		{
+			$('.button.send').click()
+			return false
+		}
 
-    // check if some element has focus. If it does, skip this plugin.
-    if ( $("*:focus").is("textarea, input") ) return true;
+		// check if some element has focus. If it does, skip this plugin.
+		if ($("*:focus").is("textarea, input"))
+			return true
 
-    if (rcmail.env.action == 'compose' || rcmail.env.task == 'login' || e.ctrlKey || e.metaKey) return true;
+		if (rcmail.env.action == 'compose' || rcmail.env.task == 'login' || e.ctrlKey || e.metaKey)
+			return true
 
-    if (rcmail.env.action == '') {	// list mailbox
+		console.log('SHIFT : ' + e.shiftKey)
+		console.log('KEYCODE : ' + e.keyCode)
+		//BOITE DE RECEPTION
+		if (rcmail.env.action == '')
+		{
+			//? = help
+			if (e.shiftKey == true && e.keyCode == 188)
+			{
+				$('#keyboard_shortcuts_help').dialog('open')
+				return false
+			}
 
-      if(rcmail.env.ks_functions[e.which]) {
-        this[rcmail.env.ks_functions[e.which]]();
-        return false;
-      }
-      
-      var ks_searchbox = '#quicksearchbox';
-      if ($('#mailsearchform').length) {    // correct HTML ID for elastic theme
-        ks_searchbox = '#mailsearchform';
-      }
-      
-      switch (e.which) {
-        case 47:		// / = search
-          $(ks_searchbox).focus();
-          $(ks_searchbox).select();
-          return false;
-        case 63:		// ? = help
-          //keyboard_shortcuts_show_help();
-          var ks_function = rcmail.env.ks_functions[e.which];
-          this[ks_function]();
-          return false;
-        case 65:		// A = mark all as read
-          rcmail.command('select-all', 'page');
-          rcmail.command('mark', 'read');
-          return false;
-        case 67:                // C = collapse-all
-          rcmail.command('collapse-all');
-          return false;
-        case 69:                // E = expand-all
-          rcmail.command('expand-all');
-          return false;
-        case 82:		// R = reply-all
-          if (rcmail.message_list.selection.length == 1)
-          rcmail.command('reply-all');
-          return false;
-        case 85:                // U = expand-unread
-          rcmail.command('expand-unread');
-          return false;
-        case 97:		// a = select all
-          rcmail.command('select-all', 'page');
-          return false;
-        case 99:		// c = compose
-          rcmail.command('compose');
-          return false;
-        case 100:		// d = delete
-          rcmail.command('delete', '', rcmail);
-          return false;
-        case 102:		// f = forward
-          if (rcmail.message_list.selection.length == 1)
-          rcmail.command('forward');
-          return false;
-        case 106:		// j = previous page (similar to Gmail)
-          rcmail.command('previouspage');
-          return false;
-        case 107:		// k = next page (similar to Gmail)
-          rcmail.command('nextpage');
-          return false;
-        case 109:		// m = mark read/unread (similar to Thunderbird)
-          var uid = rcmail.message_list.get_selection();
-          if (uid && uid.length > 0) {
-            var mid = rcmail.message_list.rows[uid[0]].id;
-            if ($('tr#' + mid).hasClass('unread')) {
-              rcmail.command('mark', 'read');
-            } else {
-              rcmail.command('mark', 'unread');
-            }
-          } else {
-            return true;
-          }
-          return false;
-        case 112:		// p = print
-          if (rcmail.message_list.selection.length == 1)
-          rcmail.command('print');
-          return false;
-        case 114:		// r = reply
-          if (rcmail.message_list.selection.length == 1)
-          rcmail.command('reply');
-          return false;
-        case 115:		// s = search
-          $(ks_searchbox).focus();
-          $(ks_searchbox).select();
-          return false;
-        case 117:		// u = update (check for mail)
-          rcmail.command('checkmail');
-          return false;
-      }
-    } else if (rcmail.env.action == 'show' || rcmail.env.action == 'preview') {
-      switch (e.which) {
-        case 82:		// R = reply-all
-          rcmail.command('reply-all');
-          return false;
-        case 99:		// c = compose
-          rcmail.command('compose');
-          return false;
-        case 100:		// d = delete
-          rcmail.command('delete');
-          return false;
-        case 102:		// f = forward
-          rcmail.command('forward');
-          return false;
-        case 106:		// j = previous message (similar to Gmail)
-          rcmail.command('previousmessage');
-          return false;
-        case 107:		// k = next message (similar to Gmail)
-          rcmail.command('nextmessage');
-          return false;
-        case 112:		// p = print
-          rcmail.command('print');
-          return false;
-        case 114:		// r = reply
-          rcmail.command('reply');
-          return false;
+			//F3 = locate folder
+			if (e.shiftKey == false && e.keyCode == 114)
+			{
+				rcmail.command('plugin.contextmenu_folder.folder_locate')
+				return false
+			}
 
-      }
-    }
-  }
-});
+			//F4 = create folder
+			if (e.shiftKey == false && e.keyCode == 115)
+			{
+				rcmail.command('plugin.contextmenu_folder.contact_folder_create')
+				return false
+			}
 
-// support functions for each function we support
-function ks_help() {
-  keyboard_shortcuts_show_help();
-}
+			//F5 = copy to folder
+			if (e.shiftKey == false && e.keyCode == 116)
+				if (rcmail.message_list.get_selection().length > 0)
+				{
+					rcmail.command('plugin.contextmenu_folder.message_copy')
+					return false
+				}
+
+			//F6 = move to folder
+			if (e.shiftKey == false && e.keyCode == 117)
+				if (rcmail.message_list.get_selection().length > 0)
+				{
+					rcmail.command('plugin.contextmenu_folder.message_move')
+					return false
+				}
+
+			//A = mark all as read
+			if (e.shiftKey == true && e.keyCode == 65)
+			{
+				rcmail.command('select-all', 'page')
+				rcmail.command('mark', 'read')
+				return false
+			}
+
+			//C = collapse-all
+			if (e.shiftKey == true && e.keyCode == 67)
+			{
+				rcmail.command('collapse-all')
+				return false
+			}
+
+			//E = expand-all
+			if (e.shiftKey == true && e.keyCode == 69)
+			{
+				rcmail.command('expand-all')
+				return false
+			}
+
+			//R = reply-all
+			if (e.shiftKey == true && e.keyCode == 82)
+			{
+				if (rcmail.message_list.selection.length == 1)
+					rcmail.command('reply-all')
+				return false
+			}
+
+			//U = expand-unread
+			if (e.shiftKey == true && e.keyCode == 85)
+			{
+				rcmail.command('expand-unread')
+				return false
+			}
+
+			//a = select all
+			if (e.shiftKey == false && e.keyCode == 65)
+			{
+				rcmail.command('select-all', 'page')
+				return false
+			}
+
+			//c = compose
+			if (e.shiftKey == false && e.keyCode == 67)
+			{
+				rcmail.command('compose')
+				return false
+			}
+
+			//d = delete
+			if (e.shiftKey == false && e.keyCode == 68)
+			{
+				rcmail.command('delete', '', rcmail)
+				return false
+			}
+
+			//f = forward
+			if (e.shiftKey == false && e.keyCode == 70)
+			{
+				if (rcmail.message_list.selection.length == 1)
+					rcmail.command('forward')
+				return false
+			}
+
+			//j = previous page (similar to Gmail)
+			if (e.shiftKey == false && e.keyCode == 74)
+			{
+				rcmail.command('previouspage')
+				return false
+			}
+
+			//k = next page (similar to Gmail)
+			if (e.shiftKey == false && e.keyCode == 75)
+			{
+				rcmail.command('nextpage')
+				return false
+			}
+
+			//m = mark read/unread (similar to Thunderbird)
+			if (e.shiftKey == false && e.keyCode == 77)
+			{
+				var uid = rcmail.message_list.get_selection()
+				if (uid && uid.length > 0)
+				{
+					var mid = rcmail.message_list.rows[uid[0]].id
+					if ($('tr#' + mid).hasClass('unread'))
+						rcmail.command('mark', 'read')
+					else
+						rcmail.command('mark', 'unread')
+				}
+				else
+					return true
+			}
+
+			//p = print
+			if (e.shiftKey == false && e.keyCode == 80)
+			{
+				if (rcmail.message_list.selection.length == 1)
+					rcmail.command('print')
+				return false
+			}
+
+			//r = reply
+			if (e.shiftKey == false && e.keyCode == 82)
+			{
+				if (rcmail.message_list.selection.length == 1)
+					rcmail.command('reply')
+				return false
+			}
+
+			//s = search
+			if (e.shiftKey == false && e.keyCode == 83)
+			{
+				$('#mailsearchform').focus()
+				$('#mailsearchform').select()
+				return false
+			}
+
+			//u = update (check for mail)
+			if (e.shiftKey == false && e.keyCode == 85)
+			{
+				rcmail.command('checkmail')
+				return false
+			}
+
+			return false
+		}
+
+		//VISUALISATION DE COURRIEL
+		else if (rcmail.env.action == 'show' || rcmail.env.action == 'preview')
+		{
+			// R = reply-all
+			if (e.shiftKey == true && e.keyCode == 82)
+			{
+				rcmail.command('reply-all')
+				return false
+			}
+
+			// c = compose
+			if (e.shiftKey == false && e.keyCode == 67)
+			{
+				rcmail.command('compose')
+				return false
+			}
+
+			// d = delete
+			if (e.shiftKey == false && e.keyCode == 68)
+			{
+				rcmail.command('delete')
+				return false
+			}
+
+			// f = forward
+			if (e.shiftKey == false && e.keyCode == 70)
+			{
+				rcmail.command('forward')
+				return false
+			}
+
+			// j = previous message (similar to Gmail)
+			if (e.shiftKey == false && e.keyCode == 74)
+			{
+				rcmail.command('previousmessage')
+				return false
+			}
+
+			// k = next message (similar to Gmail)
+			if (e.shiftKey == false && e.keyCode == 75)
+			{
+				rcmail.command('nextmessage')
+				return false
+			}
+
+			// p = print
+			if (e.shiftKey == false && e.keyCode == 80)
+			{
+				rcmail.command('print')
+				return false
+			}
+
+			// r = reply
+			if (e.shiftKey == false && e.keyCode == 82)
+			{
+				rcmail.command('reply')
+				return false
+			}
+
+			return false
+		}
+	}
+})
